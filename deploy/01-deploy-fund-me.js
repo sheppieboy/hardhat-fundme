55443cb03036c3be40efd675bf7a8cb8a28efbf6
Luke Sheppard <sheppiegit@gmail.com>
2023-05-03 16:56:12 -0400

    Added a price feed network config that allows the contracts to be deployed on different chains.

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,7 +1,19 @@
+const { networkConfig } = require('../helper-hardhat-config');
+const { network } = require('hardhat');
+
 module.exports = async ({ getNameAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNameAccounts();
   const chainId = network.config.chainId;
 
-  
+  const ethUSDPriceFeed = networkConfig[chainId]['ethUSDPriceFeed'];
+
+  //if contract doesn't exist, we deploi a min version for our local network
+
+  //When using a localhost or hardhat network we want to use a mock
+  const fundMe = await deploy('FundMe', {
+    from: deployer,
+    args: [], // put price feed address
+    log: true,
+  });
 };
