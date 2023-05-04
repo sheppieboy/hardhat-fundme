a00abfcdb6dc2635522a914bb39568f0bf80a140
Luke Sheppard <sheppiegit@gmail.com>
2023-05-04 13:14:41 -0400

    Added a verify to the deployment script.

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@ const {
   developmentChains,
 } = require('../helper-hardhat-config');
 const { network } = require('hardhat');
+const { verify } = require('../utils/verify');
 
 module.exports = async ({ getNameAccounts, deployments }) => {
   const { deploy, log } = deployments;
@@ -21,11 +22,19 @@ module.exports = async ({ getNameAccounts, deployments }) => {
   //if contract doesn't exist, we deploi a min version for our local network
 
   //When using a localhost or hardhat network we want to use a mock
+  const args = [ethUSDPriceFeedAddress];
   const fundMe = await deploy('FundMe', {
     from: deployer,
-    args: [ethUSDPriceFeedAddress], // put price feed address
+    args: args, // put price feed address
     log: true,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
   log('-----------------------------------------');
 };
 module.exports.tags = ['all', 'fundme'];
