430dd5e76d9b3dc93c2447b0112408baa9076758
Luke Sheppard <sheppiegit@gmail.com>
2023-05-04 13:03:48 -0400

    Finished the 01-deploye fund me contract script.

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,4 +1,7 @@
-const { networkConfig } = require('../helper-hardhat-config');
+const {
+  networkConfig,
+  developmentChains,
+} = require('../helper-hardhat-config');
 const { network } = require('hardhat');
 
 module.exports = async ({ getNameAccounts, deployments }) => {
@@ -6,14 +9,23 @@ module.exports = async ({ getNameAccounts, deployments }) => {
   const { deployer } = await getNameAccounts();
   const chainId = network.config.chainId;
 
-  const ethUSDPriceFeed = networkConfig[chainId]['ethUSDPriceFeed'];
+  let ethUSDPriceFeedAddress;
+
+  if (developmentChains.includes(network.name)) {
+    const ethUSDAggregator = await deployments.get('MockV3Aggregator');
+    ethUSDPriceFeedAddress = ethUSDAggregator.address;
+  } else {
+    ethUSDPriceFeedAddress = networkConfig[chainId]['ethUSDPriceFeed'];
+  }
 
   //if contract doesn't exist, we deploi a min version for our local network
 
   //When using a localhost or hardhat network we want to use a mock
   const fundMe = await deploy('FundMe', {
     from: deployer,
-    args: [], // put price feed address
+    args: [ethUSDPriceFeedAddress], // put price feed address
     log: true,
   });
+  log('-----------------------------------------');
 };
+module.exports.tags = ['all', 'fundme'];
